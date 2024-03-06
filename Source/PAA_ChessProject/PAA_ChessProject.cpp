cba55b720afa59c38c73cedf0dd9c71298f458ac
AleSimeoni02 <alessiosimeoni2002@gmail.com>
2024-03-06 12:01:33 +0100

    First commit

---
+++ Source/PAA_ChessProject/PAA_ChessProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PAA_ChessProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PAA_ChessProject, "PAA_ChessProject" );
---
+++ Source/PAA_ChessProject/PAA_ChessProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PAA_ChessProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAA_CHESSPROJECT_API APAA_ChessProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
